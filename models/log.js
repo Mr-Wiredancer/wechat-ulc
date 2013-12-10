aa5648bd95c2629d81e6e6b1993951281ac6f298
Jiahao Li <lijiahao90@gmail.com>
2013-12-10 15:05:25 +0800

    t

--- models/log.js
+++ models/log.js
@@ -2,8 +2,8 @@ var mongoose = require('./model.js')
 	, Schema = mongoose.Schema;
 
 var logSchema = new Schema({
-	content_json: String
-	_id: {type:ObjectId, index:{unique:true}}
+	content_json: String,
+	"_id": {type:Schema.ObjectId, index:{unique:true}}
 }, {capped: 8000000});
 
 module.exports = mongoose.model('Log', logSchema);
