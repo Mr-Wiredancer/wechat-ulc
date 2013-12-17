b7c6875641de4daee0e617c52c495f72a300d331
Jiahao Li <lijiahao90@gmail.com>
2013-12-17 14:27:03 +0800

    fixed a wrong condition

--- middlewares/syscommand.js
+++ middlewares/syscommand.js
@@ -5,9 +5,9 @@ module.exports = function(req, res, next){
 	console.log('is system command');
 	if (msg.isRegisterCommand()){
 		if (req.isFromStaff){
-          	res.send(msg.makeResponseMessage('text', '[SYS]您已成功注册成为客服').toXML());
-		}else{
       		res.send(msg.makeResponseMessage('text', '[SYS]请不要重复注册').toXML());
+		}else{
+          	res.send(msg.makeResponseMessage('text', '[SYS]您已成功注册成为客服').toXML());
 		}
 	}else {
 		res.send(msg.makeResponseMessage('text', '[SYS]'+msg.toJSON()).toXML()); 
