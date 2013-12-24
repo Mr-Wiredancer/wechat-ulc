5e6eba050e9058915f7d2c049ac80bcce9cd5a5c
Jiahao Li <lijiahao90@gmail.com>
2013-12-24 09:54:56 +0800

    new app secret; logger mode back to dev

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var express = require('express')
 var app = express()
   , APPID = 'wx536ca9a0d796f541'
   // , APPID = 'wx07031480fe88fbf6' //TEST ACCOUNT
-  , APPSECRET = '6b02a04187dcc4567f0f683a5e081773'
+  , APPSECRET = '5c4a52af334f85ec01ad3060e1551b8b'
   // , APPSECRET = 'f73012337cad2ab2a8008cb27b54775c' //TEST ACCOUNT
   , requestify = require('requestify');
 
@@ -44,7 +44,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('default'));
+app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
